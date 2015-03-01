ee2022c1be442eb7929832f61a0118bef485c4f1
Christoph Deil <Deil.Christoph@gmail.com>
2015-03-02 00:47:38 +0100

    Add tests

--- setup.py
+++ setup.py
@@ -5,6 +5,31 @@ def readme():
     with open('README.rst') as f:
         return f.read()
 
+# http://pytest.org/latest/goodpractises.html#integration-with-setuptools-test-commands
+import sys
+
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
 setup(name='pyjs9',
       version='1.0',
       description='Python/JS9 connection, with numpy and astropy/fits support',
@@ -23,4 +48,7 @@ setup(name='pyjs9',
       url='http://js9.si.edu',
       license='MIT',
       packages=['pyjs9', 'pyjs9.extern'],
-      zip_safe=False)
+      zip_safe=False,
+      tests_require=['pytest', 'selenium'],
+      cmdclass={'test': PyTest},
+)

923825d26df854dcd75e71a587cad380d226d1d4
Eric Mandel <eric@cfa.harvard.edu>
2020-12-14 08:22:11 -0500

    public release v3.3

--- pyjs9/__init__.py
+++ pyjs9/__init__.py
@@ -25,7 +25,7 @@ pyjs9.py connects Python and JS9 via the JS9 (back-end) helper
 """
 
 # pyjs9 version
-__version__ = '3.2'
+__version__ = '3.3'
 
 # try to be a little bit neat with global parameters
 js9Globals = {}
